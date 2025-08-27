01c9291ce4d9640a5530dd316d335fc215e5db26
Zawn-Tsu <nguyenvantu3425@gmail.com>
2025-08-27 23:18:15 +0700

    Day02_lab

---
+++ BTTH_Day02/BTTH_Day02/Controllers/2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTTH_Day02.Controllers
+{
+    public class _2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
---
+++ BTTH_Day02/BTTH_Day02/Controllers/MyNewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTTH_Day02.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Sample");
+        }
+        public IActionResult Sample()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
